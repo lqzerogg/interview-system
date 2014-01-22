c071225cbd56be7470ce1d5a8c3babbf5318a735
luochuangjie <luochuangjie@lightinthebox.com>
2014-01-22 15:59:42 +0800

    third edition, change a little

--- interview/static/base.js
+++ interview/static/base.js
@@ -3,4 +3,19 @@ jQuery(function($) {
 		e.preventDefault();
 		window.history.go(-1);
 	});
+	var waitingTpl = '<div class="waiting">\
+						<div class="container">\
+							<div class="row">\
+								<div class="col-md-6 col-md-offset-3">\
+									<div class="alert alert-info">\
+										<h1>commiting, please wait.</h1>\
+									</div>\
+								</div>\
+							</div>\
+						</div>\
+					</div>';
+
+	$(document).on('click', '.btn-commit', function(e) {		
+		// $('body').prepend(waitingTpl);
+	});
 });
